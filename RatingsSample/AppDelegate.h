502965de0a99905d127413ba14cf8dd3d4fbf1fa
parag-deshpande <parag.deshpande@klouddata.com>
2016-12-21 15:25:11 +0530

    init

---
+++ RatingsSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RatingsSample
+//
+//  Created by Parag on 12/15/16.
+//  Copyright © 2016 KloudData. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RatingsSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RatingsSample
+//
+//  Created by Parag on 12/15/16.
+//  Copyright © 2016 KloudData. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
